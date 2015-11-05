36abf015ff3c470d29216bb08d5836467f5fc9cf
MohammedAbuissa <mohammed.abuissa@outlook.com>
2015-11-05 02:11:07 +0200

    Graph Class finished not tested

--- Handasa-Map/Handasa-Map/Graph.cs
+++ Handasa-Map/Handasa-Map/Graph.cs
@@ -6,7 +6,49 @@ using System.Threading.Tasks;
 
 namespace Handasa_Map
 {
-    class Graph
+    class Graph<T>
     {
+        private Dictionary<T, LinkedList<T>> Data = new Dictionary<T, LinkedList<T>>();
+        private bool Directed;
+        public Graph(bool Directed){
+            this.Directed = Directed;
+        }
+        public void AddNode (T Node){
+            Data.Add(Node, new LinkedList<T>());
+        }
+        public void AddEdge(T From, T To){
+            Data[From].AddLast(To);
+            if (!Directed)
+                Data[To].AddLast(From);
+        }
+        public LinkedList<T> DFS(T Source, T Destination,T Parent)
+        {
+            Stack<T> OrderOfVisit = new Stack<T>();
+            Dictionary<T, T> ParentChildTable = new Dictionary<T, T>();
+            List<T> Visited = new List<T>();
+            T P = Parent;
+            T S = Source;
+            //ordinary traversal
+            while (!(S as IEquatable<T>).Equals(Destination))
+            {
+                if (!Visited.Contains(S))
+                {
+                    ParentChildTable[S] = P;
+                    Visited.Add(S);
+                    foreach (T child in Data[S])
+                        OrderOfVisit.Push(child);
+                }
+                P = S;
+                S = OrderOfVisit.Pop();
+            }
+            //traverse back parents
+            LinkedList<T> Route = new LinkedList<T>();
+            do
+            {
+                Route.AddLast(S);
+                S = ParentChildTable[S];
+            } while (!(S as IEquatable<T>).Equals(Source));
+            return Route;
+        }
     }
 }
