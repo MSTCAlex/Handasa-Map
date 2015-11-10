0894105c028c6dddc89fc23f1a068d9f822fd11e
ِAmr Alaa <amr.alaa20191404@gmail.com>
2015-11-10 11:08:18 +0200

    extract data and format it to get description, place

--- Handasa-Map/Helper/SaveTimeTable.cs
+++ Handasa-Map/Helper/SaveTimeTable.cs
@@ -20,5 +20,36 @@ namespace Handasa_Map.Helper
         // for example if I wanna know what i have in Thursday Third period
         // string descriptionAndPlace = timetableholder.values[Thursday::Third];
         //then we can format descriptionAndPlace string to get the place
-    }
-}
+
+
+        //first we get day/priod data
+        public string priodData(string day, string priod)
+        {
+
+            //extract data from local settings
+            Windows.Storage.ApplicationDataContainer timetableHolder = Windows.Storage.ApplicationData.Current.LocalSettings;
+            string priodData = timetableHolder.Values[day + "::" + priod] as string;
+
+            if (priodData == null)
+            {
+                return "You have nothing in this priod";
+            }
+
+            return priodData;
+        }
+
+        //then we format priod data to get description and place
+           // --- GET Description
+        public string description(string priodData)
+        {
+            string[] data = priodData.Split(new string[] { "::" },System.StringSplitOptions.None);
+            return data[0];
+        }
+
+           // --- GET Place
+        public string place(string priodData)
+        {
+            string[] data = priodData.Split(new string[] { "::" }, System.StringSplitOptions.None);
+            return data[1];
+        }
+
